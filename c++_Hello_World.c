3f76fcfcf8c34c3e664d9f8b5d0f51240312503c
gabboraron <gabboraron@gmail.com>
2018-02-23 21:19:11 +0100

    Add files via upload

---
+++ c++_Hello_World.c
@@ -0,0 +1,12 @@
+// Hello world c++
+#include <iostream>
+
+//using namespace std;
+
+int main()
+{
+  //  cout << "Hello world!" << endl;
+  std::cout<<"Hello World!"<<std::endl;
+    return 0;
+}
+
---
+++ c_Hello_World.c
@@ -0,0 +1,7 @@
+/*Hello World C-ben */
+#include <stdio.h>
+int main()
+{
+    Printf("Hello World");
+    return 0;
+}
